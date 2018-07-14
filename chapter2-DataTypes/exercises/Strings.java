54434e35ea65c87d449f57176b52e4775a9f5af0
Somayeh <mail.somayeh@gmail.com>
2018-07-14 13:46:41 +0100

    Declare string variable and print message contain variables

--- chapter2-DataTypes/exercises/Strings.java
+++ chapter2-DataTypes/exercises/Strings.java
@@ -1,12 +1,12 @@
 public class Strings {
   public static void main(String[] args) {
     // declare the first name
-
+    String name1 = "John";
     // declare the second name
-
+    String name2 = "Jane";
     // create your message
 
     // print your message!
-
+    System.out.println(name1 + " and " + name2 + " are learning Java!");
   }
 }
