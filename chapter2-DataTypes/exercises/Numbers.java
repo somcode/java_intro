9353365fda971dae3d25f32e29e674c66eedd866
Somayeh <mail.somayeh@gmail.com>
2018-07-14 14:00:05 +0100

    Declare integer and double variable and print result of adding them

--- chapter2-DataTypes/exercises/Numbers.java
+++ chapter2-DataTypes/exercises/Numbers.java
@@ -1,12 +1,12 @@
 public class Numbers {
   public static void main(String[] args) {
     // declare an integer variable with a value
-
+    int num1 = 9;
     // declare a double variable with a value
-
+    double num2 = 14.7;
     // store the result of adding the two together in a variable
-
+    double result = num1 + num2;
     // print it
-
+    System.out.println(result);
   }
 }
